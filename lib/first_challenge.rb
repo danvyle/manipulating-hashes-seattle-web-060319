560eb1a83773bc73b57d1c1ba700393cf2646a5c
Dan-Vy Le <danvyle85@gmail.com>
2019-05-22 21:06:06 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -24,7 +24,7 @@ def first_challenge
 end
 
 
-  #remember to return your newly altered contacts hash!
-  contacts
-end
+#   #remember to return your newly altered contacts hash!
+#   contacts
+# end
 
